c915d1e2adcf002d220f194ef007ff7e36ebb775
Caio Scatolino <caio.scatolino@outlook.com>
2025-04-09 09:09:32 -0300

    find, findOne, findById

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -6,7 +6,19 @@ import User from '../models/User';
 export const home = async (req: Request, res: Response) => {
 
     let usuarios = await User.find({})
+
+    usuarios.map(user => {
+        console.log(user.name.firstName, user.name.lastName);
+    });
+
     console.log("USUARIOS", usuarios);
+
+    let usuario = await User.findOne({
+        email: 'caio@outlook.com',
+        age: { $gte: 18, $lt: 100 } // greater than or equal to 18 and less than 100
+    })
+
+    console.log("USUARIO", usuario);
 
     let age: number = 90;
     let showOld: boolean = false;
